fc7d896f55335535aef6f16b5c96af94183223a7
Thaumatized <veeti.junkkala@gmail.com>
2026-06-21 23:16:00 +0300

    feat: Basic menu

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,50 +1,109 @@
 using System;
 using Godot;
-using Godot.Collections;
+
+public enum Country
+{
+	Finland,
+	UnitedStatesOfAmerica,
+}
+
+public struct ServerInfo
+{
+	public Country country;
+	public string ipOrDomain;
+	public int port;
+
+	public override string ToString() => $"({country} - {ipOrDomain}:{port})";
+
+	public ServerInfo(Country country, string ipOrDomain, int port)
+	{
+		this.country = country;
+		this.ipOrDomain = ipOrDomain;
+		this.port = port;
+	}
+}
 
 public partial class MainMenu : Control
 {
+	const int maxPlayers = 32;
+
 	//http://www.iana.org/assignments/port-numbers
 	//https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt
 	//10056-10079 are unassigned
 
-	const int port = 10056;
-	const int maxPlayers = 32;
+	ServerInfo[] serverInfos =
+	[
+		new ServerInfo(Country.Finland, "hydrogen.thaumatized.com", 10056),
+		new ServerInfo(Country.Finland, "helium.thaumatized.com", 10056),
+		new ServerInfo(Country.Finland, "lithium.thaumatized.com", 10056),
+		new ServerInfo(Country.Finland, "beryllium.thaumatized.com", 10056),
+	];
 
-	//const string SERVER_IP = "192.168.1.213";
-	//const string SERVER_IP = "127.0.0.1";
-	const string SERVER_IP = "hydrogen.thaumatized.com";
+	OptionButton serverSelect;
+	LineEdit ipOrDomainField;
+	LineEdit portField;
 
-	public void play()
+	public override void _Ready()
+	{
+		serverSelect = this.GetNode<OptionButton>("serverOption");
+		ipOrDomainField = this.GetNode<LineEdit>("IpOrDomain");
+		portField = this.GetNode<LineEdit>("Port");
+
+		foreach (ServerInfo info in serverInfos)
+		{
+			Texture2D flag = ResourceLoader.Load<Texture2D>(
+				"res://2DTextures/CountryFlags/" + info.country.ToString() + ".png"
+			);
+			serverSelect.AddIconItem(flag, info.ipOrDomain);
+		}
+
+		selectServer(0);
+
+		base._Ready();
+	}
+
+	void selectServer(int index)
+	{
+		ipOrDomainField.Text = serverInfos[index].ipOrDomain;
+		portField.Text = serverInfos[index].port.ToString();
+	}
+
+	string getIpOrDomain()
+	{
+		return ipOrDomainField.Text;
+	}
+
+	int getPort()
+	{
+		if (int.TryParse(portField.Text, out int port))
+			return port;
+		else
+			return 10056;
+	}
+
+	public void join()
 	{
 		ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
-		Error error = peer.CreateClient(SERVER_IP, port);
+		Error error = peer.CreateClient(getIpOrDomain(), getPort());
 		GD.Print(error);
 		Multiplayer.MultiplayerPeer = peer;
 
 		GetTree().ChangeSceneToFile("res://Scenes/match.tscn");
 	}
 
-	public void startServer()
+	public void host()
 	{
 		ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
-		Error error = peer.CreateServer(port, maxPlayers);
+		Error error = peer.CreateServer(getPort(), maxPlayers);
 		GD.Print(error);
 		Multiplayer.MultiplayerPeer = peer;
 
 		GetTree().ChangeSceneToFile("res://Scenes/match.tscn");
 	}
 
-	public void printPeers()
+	public void exitGame()
 	{
-		foreach (Dictionary signal in GetTree().GetSignalList())
-		{
-			GD.Print(signal["name"]);
-		}
-		foreach (int peerId in Multiplayer.GetPeers())
-		{
-			GD.Print(peerId);
-		}
+		GetTree().Quit();
 	}
 
 	public void connectionStatus(Error error)
