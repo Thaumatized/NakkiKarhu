cc6700313bae4462844c9b3d10195e87b83a550c
Thaumatized <veeti.junkkala@gmail.com>
2026-06-23 22:28:19 +0300

    feat: Disable collisions after hitting something

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -6,6 +6,8 @@ public partial class Projectile : RigidBody3D
 	public float force = 10f;
 	public float verticalForce = 10f;
 
+	public Vector3 lastVelocity = Vector3.Forward;
+
 	public override void _Ready()
 	{
 		this.BodyEntered += onHit;
@@ -22,9 +24,15 @@ public partial class Projectile : RigidBody3D
 			GD.Print($"{body.GetPath()} was player");
 			//player.Velocity += (player.Position - this.Position).Normalized() * force + Vector3.Up * verticalForce
 			player.Velocity +=
-				this.LinearVelocity.Normalized() * force + Vector3.Up * verticalForce;
+				this.lastVelocity.Normalized() * force + Vector3.Up * verticalForce;
 		}
 		catch { }
 		this.BodyEntered -= onHit;
+		this.SetCollisionMaskValue(2, false);
 	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+		lastVelocity = this.LinearVelocity;
+    }
 }
